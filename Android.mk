35e9f7d72d82506ad4d40cd141ecac44e03678da
picasso09 <119201284+picasso09@users.noreply.github.com>
2025-01-24 16:54:19 +0700

    rock: move radio into vendor 2/2
    Signed-off-by: picasso09 <119201284+picasso09@users.noreply.github.com>
    
    Date:      Fri Jan 24 16:54:19 2025 +0700
    Change-Id: 3e6be5cd80c14ca8017f1e5449b473373f99dea1

--- Android.mk
+++ Android.mk
@@ -6,4 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),rock)
 
+$(call add-radio-file-sha1-checked,radio/dpm.img,8f815eae457430d4db59db424e2cad561d831eb7)
+$(call add-radio-file-sha1-checked,radio/gz.img,1585e327e26f2da5b9858bfbb77d1e2bdfc52dd0)
+$(call add-radio-file-sha1-checked,radio/lk.img,56d236b57fdbfe91e02589d43a591b89c60a61ed)
+$(call add-radio-file-sha1-checked,radio/logo.img,1b1735e1117d8193f1b17b8d7aa9dca67f95ace0)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,116dacd3e3b4d22664ebfd31299353ce4b7520f9)
+$(call add-radio-file-sha1-checked,radio/md1img.img,88feeadbbb0889a69c5f7cb408c4b2ecf05d1c57)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,2ef9c701511d2edd5931e3742b803ed6d8184b80)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,7b98750858165a58ebfa65bfe7c01112c5198efd)
+$(call add-radio-file-sha1-checked,radio/scp.img,bc5a76e5daeab4b6279e2726176bf59c8313fb81)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,94211c88f2c500f52cf5f266d44822e7d75b0d9b)
+$(call add-radio-file-sha1-checked,radio/sspm.img,4f9b75ed90c416b9f6333087bc42f4c11f4f1875)
+$(call add-radio-file-sha1-checked,radio/tee.img,08ca82f0668cbdf4eb0bf6687251aded4e2c1e0c)
+
 endif
