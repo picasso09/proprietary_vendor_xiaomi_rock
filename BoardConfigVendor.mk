9d01e9157b770da3c724e5e3187ef65097e348bf
picasso09 <picasso09DV@proton.me>
2025-12-22 20:38:33 +0700

    rock: sync from UP1A.231005.007 V816.0.12.0 device tree
    Signed-off-by: picasso09 <picasso09DV@proton.me>

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,17 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    dpm \
+    gz \
+    lk \
+    logo \
+    mcupm \
+    md1img \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee
